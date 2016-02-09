bc379960b1189e3033e55f6f1a6a2117f1bdc839
matt mcgraw <matthew.mcgraw@gmail.com>
2016-02-09 10:24:44 -0500

    adding second test, which fails

--- ruby/tests_pmt_solver.rb
+++ ruby/tests_pmt_solver.rb
@@ -2,17 +2,33 @@ require "minitest/autorun"
 require_relative "pmt_solver"
 
 class TestPaymentSolver < MiniTest::Unit::TestCase
-    def setup
+    # def setup
+    #     values = {  "beginning_value": 100000,
+    #                 "future_value": 0,
+    #                 "interest_rate": 0.08,
+    #                 "payment_frequency": "years",
+    #                 "periods": 25}
+    #     @loan = Loan.new(values)
+    # end
+
+    def test_loan_one
         values = {  "beginning_value": 100000,
                     "future_value": 0,
                     "interest_rate": 0.08,
                     "payment_frequency": "years",
                     "periods": 25}
-        @loan = Loan.new(values)
+        loan1 = Loan.new(values)
+        assert_equal 9367.88, loan1.pmt
     end
 
-    def test_loan_one
-        assert_equal 9367.88, @loan.pmt
+    def test_loan_two
+        values = {  "beginning_value": 100000,
+                    "future_value": 0,
+                    "interest_rate": 0.08,
+                    "payment_frequency": "months",
+                    "periods": 25}
+        loan2 = Loan.new(values)
+        assert_equal 771.82, loan2.pmt
     end
 
 end
